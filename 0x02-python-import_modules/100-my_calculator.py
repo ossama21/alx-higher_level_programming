53d4c1a82ad972c42824b371392f63984f2f8df8
ossama21 <ossamahattan@gmail@com>
2023-11-30 11:06:03 -0800

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif sys.argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    from calculator_1 import add, div, mul, sub
+
+# checks the number of arguments
+if len(sys.argv) - 1 != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+operations = {'+': add, '-': sub, '*': mul, '/': div}
+
+if sys.argv[2] not in list(operations.keys()):
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+print("{} {} {} = {}".format(a, sys.argv[2], b, operations[sys.argv[2]](a, b)))
