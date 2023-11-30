fd1751f1c5f01282bdb19ddf9705647a3b7d4692
ossama21 <ossamahattan@gmail@com>
2023-11-30 10:46:36 -0800

    all

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,4 +5,4 @@ sum = 0
 length = len(sys.argv)
 for i in range(1, length):
     sum += int(sys.argv[i])
-    print("{}".format(sum))
+print("{}".format(sum))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     import hidden_4
-    for names in dir(hidden_4):
-        if names[:2] != "__":
-            print(n)
+
+index = sorted(dir(hidden_4))
+for index, item in enumerate(index):
+    if item[:2] != '__':
+        print("{}".format(item))
