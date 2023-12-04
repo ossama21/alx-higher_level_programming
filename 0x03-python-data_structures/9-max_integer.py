7108f7eb5b119dcb734a68078a0f0c99f9035934
ossama21 <ossamahattan@gmail@com>
2023-12-03 16:29:46 -0800

    all

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_value = my_list[0]
+    for num in my_list[1:]:
+        if num > max_value:
+            max_value = num
+    return max_value
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
