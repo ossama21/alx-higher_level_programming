186fa87cd0f7baa9311e6fa8fb1b5d9a1ea97095
ossama21 <ossamahattan@gmail@com>
2023-12-04 12:26:58 -0800

    all

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,46 +1,22 @@
 #include "lists.h"
-
 /**
- * is_palindrome - checks if a singly linked list
- * is a palindrome
- * @head: pointer to head of list
- * Return: 0 if it is not a palindrome,
- * 1 if it is a palndrome
+ * is_palindrome - checks if palindrome
+ * @head: head of node
+ * Return: 0 if not, 1 if is
  */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *tortoise = *head, *hare = *head, *temp, *stack_top = NULL;
-	int n;
+	unsigned int len = 1;
+	listint_t *temp;
 
-	/* Traverse the linked list and push elements onto the stack */
-	while (hare != NULL && hare->next != NULL)
+	if (head == NULL|| *head == NULL)
+		return (1);
+
+	temp = *head;
+	while (temp) /* get len of list */
 	{
-		/* Push the value onto the stack */
-		n = tortoise->n;
-		temp = malloc(sizeof(listint_t));
-		if (temp == NULL)
-			return (0);
-		temp->n = n;
-		temp->next = stack_top;
-		stack_top = temp;
-		tortoise = tortoise->next;
-		hare = hare->next->next;
+		temp = temp->next;
+		len++;
 	}
-	/* If the length of the linked list is odd, skip the middle element */
-	if (hare != NULL)
-		tortoise = tortoise->next;
-	/* Traverse the remaining linked list and compare with the*/
-	/* elements popped from the stack */
-	while (tortoise != NULL)
-	{
-		/* Pop the value from the stack */
-		if (stack_top == NULL || stack_top->n != tortoise->n)
-			return (0);
-		temp = stack_top;
-		stack_top = stack_top->next;
-		free(temp);
-		tortoise = tortoise->next;
-	}
-	return (1);
+	return (0);
 }
