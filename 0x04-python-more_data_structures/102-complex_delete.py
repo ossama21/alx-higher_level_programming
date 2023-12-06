00992e2a5bbeea4f71961e58dd410acc66ba52c6
ossama21 <ossamahattan@gmail@com>
2023-12-06 12:31:16 -0800

    all

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,8 @@
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    return sum(map(lambda x: x[0] * x[1], my_list)) / sum(map(lambda x: x[1], my_list))
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    key_list = list(a_dictionary.keys())
+
+    for key_val in key_list:
+        if value == a_dictionary.get(key_val):
+            del a_dictionary[key_val]
+    return a_dictionary
