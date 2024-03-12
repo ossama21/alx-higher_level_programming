1a356f96c5f5f5fffcca588367746abddbb68009
ossama21 <ossamahattan@gmail.com>
2024-03-12 15:58:48 -0700

    all

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversedArray = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedArray.push(list[i]);
+  }
+  list = reversedArray;
+  return list;
+};
